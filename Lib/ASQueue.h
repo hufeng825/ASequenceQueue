8b4a71d59f5c90c59c60bce9d7e1f641ece7a79e
wangchao <cclcwangchao@gmail.com>
2012-06-12 09:22:43 +0800

    first commit

---
+++ Lib/ASQueue.h
@@ -0,0 +1,13 @@
+//
+//  ASQueue.h
+//  ASequenceQueue
+//
+//  Created by chao wang on 12-6-11.
+//  Copyright (c) 2012年 bupt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASQueue : NSObject
+
+@end
---
+++ Lib/ASQueue.m
@@ -0,0 +1,13 @@
+//
+//  ASQueue.m
+//  ASequenceQueue
+//
+//  Created by chao wang on 12-6-11.
+//  Copyright (c) 2012年 bupt. All rights reserved.
+//
+
+#import "ASQueue.h"
+
+@implementation ASQueue
+
+@end
